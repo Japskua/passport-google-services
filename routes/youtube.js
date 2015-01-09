f831b4a20b8550fc150a220b0936dcb26ef19b36
Janne Parkkila <japskua@gmail.com>
2015-01-09 16:03:27 +0200

    Managed to get youtube connection with passport authentication working. OU! YEH!

--- routes/youtube.js
+++ routes/youtube.js
@@ -1,17 +1,50 @@
 /**
  * Created by Janne on 8.1.2015.
  */
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    google = require('googleapis'),
+    OAuth2 = google.auth.OAuth2,
+    GoogleInfo = require('./../bin/google-info');
 
 
 /* GET /youtube/ */
 router.get('/', ensureAuthenticated, function(req, res){
 
+    var googleInfo = new GoogleInfo();
+
     // Try to connect to youtube here
+    var plus = google.plus('v1');
+    var oauth2client = new OAuth2();
+    console.log("OAuth Client done", oauth2client);
+    oauth2client.setCredentials({
+        access_token : req.user.token,
+        refresh_token : ""
+    });
 
+    var youtube = google.youtube('v3');
+    youtube.playlists.list({ part : "snippet", mine : true, auth : oauth2client}, function(err, response) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
 
-    res.render('youtube', { user: req.user });
+        //res.render('youtube', { user: req.user });
+        res.send(response);
+    });
+
+    // WORKING GOOGLE PLUS PART. DON'T REMOVE!
+/*
+    plus.people.get({ userId : "me", auth : oauth2client}, function(err, response) {
+        if (err) {
+            res.json({error : err});
+        }
+
+        //res.render('youtube', { user: req.user });
+        res.send(response);
+    });
+*/
+
 });
 
 // Simple route middleware to ensure user is authenticated.
